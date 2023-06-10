d3ec41b3d75f8aaecc0dd95cfd7b8e4fee0b2b4d
Roqia Ahmed <103763199+roqiaahmed@users.noreply.github.com>
2023-06-10 16:43:26 +0300

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    copy = [i for i in my_string]
-    for n in copy:
-        if n == "c" or n == "C":
-            copy.remove(n)
-    return ("".join(copy))
+   copy = [x for x in my_string if x != 'c' and x != 'C']
+   return ("".join(copy))
