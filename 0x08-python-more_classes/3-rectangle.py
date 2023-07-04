daae7c63c2b3751990f05357e7d518daf2938b5c
Roqia Ahmed <roqiaahmed447@gmail.com>
2023-07-04 23:42:11 +0300

    update

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -58,5 +58,22 @@ class Rectangle:
             return ""
         result = ""
         for i in range(self.__height):
-            result += "#" * self.__width + "\n"
+            if i != self.__height - 1:
+                result += "#" * self.__width + "\n"
+            else:
+                result += "#" * self.__width
         return result
+
+
+my_rectangle = Rectangle(2, 4)
+print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
+
+print(str(my_rectangle))
+print(repr(my_rectangle))
+
+print("--")
+
+my_rectangle.width = 10
+my_rectangle.height = 3
+print(my_rectangle)
+print(repr(my_rectangle))
