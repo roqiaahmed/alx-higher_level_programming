4352c7dbfe31f5dabfee8af01af61abd05167e94
Roqia Ahmed <roqiaahmed447@gmail.com>
2023-09-12 06:32:07 +0300

    6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+for (let i = 0; i < languages.length; i++) {
+  const element = languages[i];
+  console.log(element);
+}
