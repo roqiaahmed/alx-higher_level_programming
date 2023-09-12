9f513f7c81f9742ea68b8d8a5560e129709eb4d9
Roqia Ahmed <roqiaahmed447@gmail.com>
2023-09-12 23:15:22 +0300

    update

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -4,7 +4,7 @@ const num1 = parseInt(process.argv[2]);
 const num2 = parseInt(process.argv[3]);
 
 function add(a, b) {
-  if (a && b) {
+  if (a & b) {
     const result = a + b;
     console.log(result);
   } else {
