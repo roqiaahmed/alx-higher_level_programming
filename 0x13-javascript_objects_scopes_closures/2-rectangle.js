415331f6dd2624676a3d9f6ce2d2b2d5b96abeb8
Roqia Ahmed <roqiaahmed447@gmail.com>
2023-09-13 06:23:22 +0300

    5-square.js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 class Rectangle {
-  constructor(_w, _h) {
-    this.width = _w;
-    this.height = _h;
+  constructor(w, h) {
+    this.width = w;
+    this.height = h;
     if (this.width <= 0 || this.height <= 0 || !this.height || !this.width) {
       this.width = undefined;
       this.height = undefined;
