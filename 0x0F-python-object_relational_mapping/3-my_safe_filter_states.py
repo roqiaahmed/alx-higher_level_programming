b830641bf4849046d9edb08ee0aae3dddeca23b4
Roqia Ahmed <roqiaahmed447@gmail.com>
2023-09-15 13:52:22 +0300

    5-filter_cities.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                         password=argv[2], db=argv[3], charset="utf8")
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY states.id ASC".format(argv[4]))
+    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY states.id ASC", (argv[4], ))
     rows = cur.fetchall()
     for row in rows:
             print(row)
--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -6,10 +6,10 @@ from sys import argv
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                        password=argv[2], db=argv[3], charset="utf8")
+                        passwd=argv[2], db=argv[3], charset="utf8")
     cur = db.cursor()
-    cur.execute("SELECT cities.id cities.name states.name FROM cities"
-                "JOIN states ON cities.state_id = state.id"
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities "
+                "JOIN states ON cities.state_id = states.id "
                 "ORDER BY cities.id ASC")
     rows = cur.fetchall()
     for row in rows:
