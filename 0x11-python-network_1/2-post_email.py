203df9664757374d660d329f8dba06f9394654cf
Roqia Ahmed <roqiaahmed447@gmail.com>
2023-09-30 19:18:29 +0300

    2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
+"""displays the value of the X-Request-Id variable found in
+the header of the response.
+"""
 
 
 if __name__ == "__main__":
-    import urllib.parse
-    import urllib.request
-    import sys
+    from urllib.request import urlopen, Request
+    from urllib.parse import urlencode
+    from sys import argv
 
-    url = sys.argv[1]
-    values = {'email' : sys.argv[2]}
-
-    data = urllib.parse.urlencode(values)
-    data = data.encode('ascii') # data should be bytes
-    req = urllib.request.Request(url, data)
-    with urllib.request.urlopen(req) as response:
-        the_page = response.read()
-        print(the_page)
+    value = {"email": argv[2]}
+    request = Request(
+            argv[1], urlencode(value).encode("ascii"))
+    with urlopen(request) as response:
+        head = response.headers.get('X-Request-Id')
+        print(response.read().decode('utf-8'))
