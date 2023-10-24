6073e2f3351b9c0682546b1126ea41240defa8c8
Roqia Ahmed <roqiaahmed447@gmail.com>
2023-10-24 22:35:33 +0300

    5-request_store.js

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const fs = require("fs");
+const request = require("request");
+request(process.argv[2], (_err, _res, body) => {
+  fs.writeFile(process.argv[3], body, "utf8", (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+});
