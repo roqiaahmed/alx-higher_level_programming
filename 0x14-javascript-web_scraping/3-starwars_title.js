44601b02885993a6b7da0cd0da7a26d706952bca
Roqia Ahmed <roqiaahmed447@gmail.com>
2023-10-24 22:13:49 +0300

    3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,6 +5,6 @@ const url = "https://swapi-api.alx-tools.com/api/films/".concat(
   process.argv[2]
 );
 request(url, (body) => {
-  film = JSON.parse(body);
+  film = JSON.parse(_err, _res, body);
   console.log(film.title);
 });
