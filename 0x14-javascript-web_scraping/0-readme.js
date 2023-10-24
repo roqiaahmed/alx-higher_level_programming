b5ec1505a17f0cf9e9b3f2486499e68a5ab9e7b9
Roqia Ahmed <roqiaahmed447@gmail.com>
2023-10-24 21:17:38 +0300

    0-readme.js

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// import fs from "fs";
+
+const fs = require("fs");
+const process = require("process");
+fs.readFile(process.argv[2], "utf8", (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
