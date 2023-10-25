be05febed968f1c0fed66d384b6d4ff47369ed58
Roqia Ahmed <roqiaahmed447@gmail.com>
2023-10-25 15:46:30 +0300

    100-starwars_characters.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -7,8 +7,9 @@ request(flm_url, (_err, _res, body) => {
   const characters = JSON.parse(body).characters;
   for (let i = 0; i < characters.length; i++) {
     const character_url = characters[i];
-    request(character_url, _err, _res, body);
-    const character = JSON.parse(body).name;
-    console.log(character);
+    request(character_url, (_err, _res, body) => {
+      const character = JSON.parse(body).name;
+      console.log(character);
+    });
   }
 });
