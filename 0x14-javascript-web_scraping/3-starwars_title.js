72d22abbbf55a151c63dda6c9a21c8eadb90d776
Roqia Ahmed <103763199+roqiaahmed@users.noreply.github.com>
2023-10-25 11:38:32 +0300

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,6 +3,6 @@
 const request = require('request');
 const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
 request(url, (_err, _res, body) => {
-  let film = JSON.parse(body);
+  const film = JSON.parse(body);
   console.log(film.title);
 });
