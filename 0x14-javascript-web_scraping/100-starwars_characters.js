d6ff54263575111d6a20270486d5666057b6e8bf
Roqia Ahmed <roqiaahmed447@gmail.com>
2023-10-25 15:45:42 +0300

    100-starwars_characters.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -3,11 +3,12 @@
 const request = require("request");
 const flm_url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
 
-request(flm_url, (err, _res, body) => {
+request(flm_url, (_err, _res, body) => {
   const characters = JSON.parse(body).characters;
   for (let i = 0; i < characters.length; i++) {
     const character_url = characters[i];
-    const character = JSON.parse(character_url).name;
+    request(character_url, _err, _res, body);
+    const character = JSON.parse(body).name;
     console.log(character);
   }
 });
