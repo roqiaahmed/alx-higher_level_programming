0637d4685ff876556681615074aa96859e01b044
Roqia Ahmed <103763199+roqiaahmed@users.noreply.github.com>
2023-10-25 11:31:28 +0300

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const fs = require("fs");
-const process = require("process");
+const fs = require('fs');
+const process = require('process');
 
-fs.readFile(process.argv[2], "utf8", (err, data) => {
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
   if (err) {
     console.error(err);
     return;
