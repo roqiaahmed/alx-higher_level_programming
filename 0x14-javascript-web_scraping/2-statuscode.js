e003eba82f8b8465291c3377acc94272f74ce44b
Roqia Ahmed <103763199+roqiaahmed@users.noreply.github.com>
2023-10-25 11:33:58 +0300

    Update 2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-const request = require("request");
+const request = require('request');
 
 request(process.argv[2], function (_err, res) {
-  console.log("code:", res.statusCode); // Print the response status code if a response was received
+  console.log('code:', res.statusCode); // Print the response status code if a response was received
 });
