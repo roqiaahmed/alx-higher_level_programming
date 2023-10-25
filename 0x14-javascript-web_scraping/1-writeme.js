41e8e3b5ac1fc9b91488aa40cdc3ef9f723629f0
Roqia Ahmed <103763199+roqiaahmed@users.noreply.github.com>
2023-10-25 11:32:12 +0300

    Update 1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const fs = require("fs");
+const fs = require('fs');
 
-fs.writeFile(process.argv[2], process.argv[3], "utf8", (err) => {
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', (err) => {
   if (err) {
     console.error(err);
   }
