b3ccc9eb9b579cba4ea3e6b3e2b38e14e0952e31
Roqia Ahmed <103763199+roqiaahmed@users.noreply.github.com>
2023-10-25 11:34:20 +0300

    Update 5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const fs = require("fs");
-const request = require("request");
+const fs = require('fs');
+const request = require('request');
 request(process.argv[2], (_err, _res, body) => {
-  fs.writeFile(process.argv[3], body, "utf8", (err) => {
+  fs.writeFile(process.argv[3], body, 'utf8', (err) => {
     if (err) {
       console.error(err);
     }
